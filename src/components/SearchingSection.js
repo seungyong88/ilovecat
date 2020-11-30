bbe1537fbef121d7f5a59fb17db1cf251f46d762
seungyong88 <gosng2001@naver.com>
2020-11-30 15:54:30 +0900

    Feature: Make recent keywords

--- src/components/SearchingSection.js
+++ src/components/SearchingSection.js
@@ -1,5 +1,6 @@
 export default class SearchingSection{
   constructor({$target, onSearch, onRandom}) {
+    this.recent = [];
     this.onSearch = onSearch;
     this.onRandom = onRandom;
     this.section = document.createElement('section');
@@ -16,6 +17,10 @@ export default class SearchingSection{
   }
 
   searchByKeyword(keyword){
+    if(keyword.length == 0) return;
+
+    this.addRecentKeyword(keyword);
+
     this.onSearch(keyword);
     this.deleteKeyword();
   }
@@ -25,7 +30,17 @@ export default class SearchingSection{
     searchBox.value = '';
   }
 
+  addRecentKeyword(keyword) {
+    if(this.recent.includes(keyword)) return;
+    if(this.recent.length == 5) this.recent.shift();
+
+    this.recent.push(keyword);
+    this.render();
+  }
+
   render() {
+    this.section.innerHTML = '';
+
     const searchingWrapper = document.createElement('div');
     searchingWrapper.className = 'searching-wrapper';
 
@@ -37,6 +52,9 @@ export default class SearchingSection{
     searchBox.className = 'search-box';
     searchBox.placeholder = '고양이를 검색하세요.';
 
+    const recentKeywords = document.createElement('div');
+    recentKeywords.className = 'recent-keywords';
+
     randomBtn.addEventListener('click', e => this.searchRandomCats());
     searchBox.addEventListener('keyup', e=> {
       if(e.keyCode == 13) {
@@ -44,10 +62,21 @@ export default class SearchingSection{
       }
     })
 
+    this.recent.forEach(keyword => {
+      const keywordLink = document.createElement('span');
+      keywordLink.className = 'keyword-link';
+      keywordLink.innerText = keyword;
+
+      keywordLink.addEventListener('click', () => this.searchByKeyword(keyword));
+
+      recentKeywords.appendChild(keywordLink);
+    })
+
     searchingWrapper.appendChild(randomBtn);
     searchingWrapper.appendChild(searchBox);
 
     this.section.appendChild(searchingWrapper);
+    this.section.appendChild(recentKeywords);
   }
 
 }
