1aa0b317633c599fc2a3dc440544785d0f4bfad1
seungyong88 <gosng2001@naver.com>
2020-11-30 15:10:09 +0900

    Feature: Notice no-data

--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -19,12 +19,24 @@ export default class ResultSection{
   }
 
   render() {
+    if(this.data == null) return;
+
     this.section.innerHTML = '';
 
-    if(this.data) {
+    if(this.data.length > 0) {
       this.data.forEach(cat => {
         new Card({$target: this.section, data: cat, onClick: this.onClick});
       })
+    }else{
+      const noDataWrapper = document.createElement('div');
+      noDataWrapper.className="no-data-wrapper";
+      
+      const noData = document.createElement('p');
+      noData.className="no-data";
+      noData.innerText = '검색 결과가 없습니다.';
+      
+      noDataWrapper.appendChild(noData);
+      this.section.appendChild(noDataWrapper);
     }
 
   }
